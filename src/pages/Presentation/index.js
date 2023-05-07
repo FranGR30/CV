6b6277fe3ba599c59a97f5fe9c8e86eafa2b5560
Fran Garcia <fgr304@gmail.com>
2023-05-07 20:14:28 -0300

    primer commit

---
+++ src/pages/Presentation/index.js
@@ -0,0 +1,90 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// Routes
+import routes from "routes";
+import SignIn from "layouts/pages/authentication/sign-in";
+
+// Images Background
+import bgImage from "assets/images/bg-presentation.jpg";
+
+function Presentation() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "/pages/authentication/sign-in",
+          label: "log in",
+          color: "info",
+          component: <SignIn />,
+        }}
+        sticky
+      />
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="white"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              CV Developer{" "}
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="white"
+              textAlign="center"
+              px={{ xs: 6, lg: 12 }}
+              mt={1}
+            >
+              Más de 10 años de experiencia en el mercado
+            </MKTypography>
+          </Grid>
+        </Container>
+      </MKBox>
+    </>
+  );
+}
+
+export default Presentation;
